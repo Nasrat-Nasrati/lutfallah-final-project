9027755fcf1e6529012b6bf2d6ea47e98c25053e
Nasrat-Nasrati <ehnn2020@gmail.com>
2024-09-25 14:26:30 +0430

    added final project of LutAllah

---
+++ headerNav.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+
+
+<!--Banner-->
+	<body>
+
+<header>
+	<a href="#" class="logo">Magic House</a>
+	
+	<input type="checkbox" id="menu-bar">
+	<label for="menu-bar">Menu </label>
+	
+	<nav class="navbar">
+	<ul>
+	 <li class="show"><a href="#">Search</a></li>
+	 <li><a href="#">Home</a></li>
+	 <li><a href="#">Opportunities+</a>
+		<ul>
+			<li><a href="#">Jobs</a></li>
+			<li><a href="Scholorships.php">Scholorships</a>
+			</li>
+		</ul>
+	 </li>
+	 <li><a href="#">Essay+</a>
+		<ul>
+		<li><a href="blog.php">Blogs</a></li>
+		<li><a href="#">Services</a></li>
+		</ul>
+	 </li>
+	 <li><a href="#">About+</a>
+		<ul>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Services</a></li>
+		</ul>
+	 </li>
+	 <li><a href="#">Log In</a></li>
+	</ul>
+</header>
+
+</body>
+
+</html>
